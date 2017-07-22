eb745d60ccceadb3d98006d7930378766baa627d
Alan Platt <aplatt@equalexperts.com>
2017-07-22 13:31:49 +0100

    make networking script more verbose when cleaning up network files

--- scripts/ubuntu/networking.sh
+++ scripts/ubuntu/networking.sh
@@ -7,10 +7,10 @@ if [ "$major_version" -le "16" ]; then
   echo "Disabling automatic udev rules for network interfaces in Ubuntu"
   # Disable automatic udev rules for network interfaces in Ubuntu,
   # source: http://6.ptmc.org/164/
-  rm -f /etc/udev/rules.d/70-persistent-net.rules
-  mkdir -p /etc/udev/rules.d/70-persistent-net.rules
-  rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
-  rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*
+  rm -vf /etc/udev/rules.d/70-persistent-net.rules
+  mkdir -vp /etc/udev/rules.d/70-persistent-net.rules
+  rm -vf /lib/udev/rules.d/75-persistent-net-generator.rules
+  rm -vrf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*
 fi
 
 # Adding a 2 sec delay to the interface up, to make the dhclient happy
