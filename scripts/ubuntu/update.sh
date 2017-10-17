007047647142e75bc02b04ac65765d2291517081
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2017-10-17 11:16:54 +0100

    [OI] temporary comment out change to release-upgrade prompt config

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -3,8 +3,8 @@
 ubuntu_version="`lsb_release -r | awk '{print $2}'`"
 ubuntu_major_version="`echo $ubuntu_version | awk -F. '{print $1}'`"
 
-# Disable release-upgrades
-sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
+## Disable release-upgrades (I don't see the benefit of this)
+#sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
 
 # Update the package list
 apt-get -y update
